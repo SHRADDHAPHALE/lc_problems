0d1edbe01b3887e1f57b9d7c9ef5e12a61f73bd1
SHRADDHAPHALE <sphale28@gmail.com>
2021-09-15 10:50:41 +0530

    added add2 function

--- src/lc_problems.py
+++ src/lc_problems.py
@@ -1,2 +1,5 @@
 def add(n):
     return n+1
+
+def add2(n):
+    return n+2
--- tests/test_lc_problem.py
+++ tests/test_lc_problem.py
@@ -1,7 +1,13 @@
-from src.lc_problems import add
+from src.lc_problems import add, add2
 
 def test_add_pass():
     assert add(1) == 2
 
 def test_add_fail():
     assert add(5) == 6
+
+def test_add2_pass():
+    assert add2(1) == 3
+
+def test_add2_fail():
+    assert add2(3) == 5
